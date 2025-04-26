19b0d559b3bd2f281af05f297ff66019643144f8
nolandex <stemapal1234@gmail.com>
2025-04-26 15:19:22 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,9 +24,18 @@
           </path>
         </svg>Download Resume</button> -->
     </div>
-    <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://drive.google.com/file/d/1-ov_TuTSGtUwTUjYgMnN3YH-lbjre7aW/view?usp=drivesdk">
-    </div>
+    <div class="flex justify-center md:justify-start fadein-right">
+  <img
+    alt="avatar"
+    fetchpriority="high"
+    width="300"
+    height="300"
+    decoding="async"
+    data-nimg="1"
+    class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict"
+    src="/assets/avatar.jpg"
+  >
+</div>
   </main>
 </template>
 
